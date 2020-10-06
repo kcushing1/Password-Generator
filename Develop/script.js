3d234ab88c809b17e17b1b43fc0c4c987af6cef0
Kasey Cushing <kasey.cushing@gmail.com>
2020-10-05 20:13:27 -0400

    code chunks work in repl.it and browser console, able to generate pw of n length

--- Develop/script.js
+++ Develop/script.js
@@ -9,23 +9,23 @@ var alphaUpper = alphaBlock.toUpperCase().split("")
 var allNumbers = numbersBlock.split("")
 var specialBlock = "!@#$%^&*?"
 var allSpecial = specialBlock.split("")
-var allCharacters = []
 var includeLower = []
 var includeUpper = []
 var includeNumbers = []
 var includeSpecial = []
+var passwordChosen = []
 /*
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  console.log("willgobacktothis")
 }
+*/
 
 function generatePassword() {
   alert("Please select from the following criteria which to be included in your secure password.")
-*/
+
   //selecting password length
   while (true){
     chosenLength = prompt("Choose a password length between 8 and 128 characters")
@@ -44,7 +44,7 @@ function generatePassword() {
     var includeLower = includeLower.concat(alphaLower)
     }
     else {
-      includeLower = null
+      includeLower = []
     }
   
   var upperCase = confirm("Would you like uppercase letters to be included")
@@ -52,7 +52,7 @@ function generatePassword() {
       var includeUpper = includeUpper.concat(alphaUpper)
     }
     else {
-        includeUpper = null
+        includeUpper = []
     }
  
   var askNumbers = confirm("Would you like numbers 0-9 to be included?")
@@ -60,7 +60,7 @@ function generatePassword() {
     var includeNumbers = includeNumbers.concat(allNumbers)
     }
     else {
-    includeNumbers = null
+    includeNumbers = []
     }
     
   var specialCharacters = confirm("Would you like special characters to be included?")
@@ -68,11 +68,33 @@ function generatePassword() {
     var includeSpecial = includeSpecial.concat(allSpecial)
     }
     else {
-    includeSpecial = null
+    includeSpecial = []
     }
-  
-console.log(includeLower,includeNumbers, includeSpecial, includeUpper)
-  var passwordLength = parseInt(chosenLength)
+//character type selection completed
+
+  var passwordLength = parseInt(chosenLength) + 1
+  var passwordArray = passwordArray.concat(includeLower, includeUpper, includeNumbers, includeSpecial)
+
+  //make sure at least one character type is chosen
+  while (true){
+  if (passwordArray.length > 10){
+    break
+  }
+  else {
+    alert("You must select at least one character type.");
+    break
+  }
+}
+
+//creating the password
+do {
+  var addUnit = passwordArray[Math.floor(Math.random()*passwordArray.length)];
+  passwordChosen.push(addUnit)
+}
+while (passwordChosen.length < passwordLength)
+
+}
+  console.log(passwordChosen.join(""))
 
 
 //code to generate goes here
@@ -81,6 +103,7 @@ console.log(includeLower,includeNumbers, includeSpecial, includeUpper)
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword){
   document.querySelector("#generate")
+  make it appear on the page
   //the function goes here
 }
 */
